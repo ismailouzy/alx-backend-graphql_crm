ee8232e84c864f0b9c1c2320ad1158fc97f5c934
ismailouzy <ismailouzal@gmail.com>
2025-06-28 18:59:00 +0100

    cleaning up structure

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 GRAPHENE = {
-    "SCHEMA": "alx_backend_graphql_crm.schema.schema"
+    "SCHEMA": "alx_backend_graphql.schema.schema"
 }
 
 
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'alx_backend_graphql_crm.urls'
+ROOT_URLCONF = 'alx_backend_graphql.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'alx_backend_graphql_crm.wsgi.application'
+WSGI_APPLICATION = 'alx_backend_graphql.wsgi.application'
 
 
 # Database
